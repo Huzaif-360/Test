a762499a1e8034759264c2f8e3b535f561a5dfec
huzaifa <huzaifa2017@namal.edu.pk>
2020-04-15 12:17:43 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpFromYouTube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
